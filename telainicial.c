c3b1ecfdace7af949e15fbcc992bbff0f0c66035
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-06-09 16:39:50 -0300

    prototipo

--- telainicial.c
+++ telainicial.c
@@ -1,8 +1,8 @@
-//PARA COMPILAR ESTE PROGRAMA USE gcc telainicial -lncurses
+//PARA COMPILAR ESTE PROGRAMA USE gcc telainicial.c -lncurses
 
 #include <curses.h> //Biblioteca grafica, necessaria a instalacao dela, comando disponivel no ReadMe do projeto
 #include <stdio.h>
-
+#include <stdlib.h>
 void sair_do_programa()
 {
 	endwin();//Finalizar a ncruses
@@ -11,24 +11,50 @@ void sair_do_programa()
 
 void tela_inicial()
 {
-	//Inicializa a biblioteca ncurses
-	initscr();  
 
-	//Inicializa o uso das cores
-	start_color();
 
-	//Pares de cor que usaremos, texto verde e fundo branco
-	init_pair(1,COLOR_GREEN,COLOR_WHITE);
+WINDOW *janela; (void)initscr();
+border(ACS_LTEE,ACS_RTEE,ACS_TTEE,ACS_BTEE, ACS_URCORNER,ACS_ULCORNER,ACS_LRCORNER,ACS_LLCORNER); 
 
-	bkgd(COLOR_PAIR(1));
+janela = newwin(LINES-10,COLS-5,(LINES-(LINES-10))/2,(COLS-(COLS- 5))/2); 
+wborder(janela, ACS_RTEE, ACS_LTEE, ACS_BTEE, ACS_TTEE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+//Inicializa a biblioteca ncurses
+initscr();  
 
-	printw("SOCIAL NETWORK");
+//Inicializa o uso das cores
+start_color();
+//Pares de cor que usaremos, texto verde e fundo branco
+init_pair(1,COLOR_GREEN,COLOR_WHITE);
 
-	move(3,1);
+//Atualiza o fundo para o par de cor 1
+bkgd(COLOR_PAIR(1));
+refresh(); 
+wrefresh(janela);
+move(2,70);
+printw("S  O  C  I  A  L        N  E  T  W  O  R  K");
+move(10,50);
+printw("LOGIN");
+move(11,100);
+printw("CADASTRAR");
+move(18,70);
+printw("SAIR");
+int ch = getch();
+switch (ch) 
+{
+    case KEY_LEFT:
+        printw("aAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        int valor;
+        scanw("%d",&valor);
+        break;
+    default:
+      break;
+}
+printw("%c",ch); 
+noecho();
+getch();
+getch();
+delwin(janela);
 
-	printw("CLIQUE EM QUALQUER TECLA PARA SAIR");
-	refresh();
-	getch();
 	sair_do_programa();
 }
 
